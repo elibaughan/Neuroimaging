cdb40878d225c37662092fa0f899fae6d1fa5a8a
elibaughan <elijahbaughan@Andrews-iPhone-010004101133.app.byu.edu>
2016-11-21 13:56:22 -0700

    job script for running freesurfer on my own brain

---
+++ Freesurfer/freesurfereli_job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --time=30:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=16384M  # memory per CPU core
+
+# Compatibility variables for PBS. Delete if not needed.
+export PBS_NODEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
+export PBS_JOBID=$SLURM_JOB_ID
+export PBS_O_WORKDIR="$SLURM_SUBMIT_DIR"
+export PBS_QUEUE=batch
+
+# Set the max number of threads to use for programs using OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD ENVIRONMENTAL VARIABLES
+export FREESURFER_HOME=/fslhome/ebaughan/apps/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+# INSERT CODE, AND RUN YOUR PROGRAMS HERE
+~/apps/freesurfer/bin/recon-all \
+-i /fslhome/ebaughan/mybrain/elibrain/elisbrain.nii.gz \
+-subjid ~/mybrain/elibrain \
+-all \
+-sd /fslhome/ebaughan/mybrain/analyses/FreeSurfer
